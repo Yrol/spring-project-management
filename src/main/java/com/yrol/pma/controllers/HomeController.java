4af64ada09ca8d8279fc0516b9578fcc3f4ee8b5
Yrol <yrollgayanth@yahoo.com>
2022-09-02 21:20:31 +1000

    Integrating Charts.js

--- src/main/java/com/yrol/pma/controllers/HomeController.java
+++ src/main/java/com/yrol/pma/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package com.yrol.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yrol.pma.dto.ProjectStageCount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,14 +32,24 @@ public class HomeController {
 	 * @GetMapping is similar to using "RequestMethod.GET" within @RequestMapping. For post use @PostMapping
 	 * */
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
+
+		Map<String, Object> map = new HashMap<>();
 		
 		List<Project> projects = proRepo.findAll();
 		List<EmployeeProject> employeesProjectCount = empRepo.employeeProjects();
+		List<ProjectStageCount> projectStageCount = proRepo.projectStageCount();
+
+		//converting the projectStageCount to JSON to be used by the home template
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		//will look like - [["NOTSTARTED", 1], ["INPROGRESS", 2], ["COMPLETED", 5]]
+		String jsonString = objectMapper.writeValueAsString(projectStageCount);
 		
 		//Adding the projects
 		model.addAttribute("projects", projects);
 		model.addAttribute("employeesProjectCount", employeesProjectCount);
+		model.addAttribute("projectStageCount", projectStageCount);
 		
 		return "/main/home";
 	}
