1a0f5fa4a7a9b7d54667a80c8ec6da9ea1cb1c31
Yrol <yrollgayanth@yahoo.com>
2022-09-26 22:08:18 +1000

    Adding spring security and creating a security configuration.

---
+++ src/main/java/com/yrol/pma/security/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package com.yrol.pma.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Authentication
+ * */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    /**
+     * Password encryption using BCrypt
+     * */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * User Authentication - In-memory
+     * Using the encrypted(BCrypt) passwords generated via: https://bcrypt-generator.com/
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user1")
+                .password("$2a$12$lyybDchRgwKOVyppgcmeIehlpe0Hgo8.XgNFjLyvFMnSpAjTlDBAi") // pass123
+                .roles("USER")
+                .and()
+                .withUser("user2")
+                .password("$2a$12$4/WSenbfyWyFZMAPsz/Nu.fqBM5rVTn6W/tjQxnWSRdkF6mxXrWv.") // pass1234
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("$2a$12$6AwjQfZVr9EUfw/4bQE1/eDSXfSkCWrNit6LIWv.xNjLnUm44edxO") // admin1234
+                .roles("ADMIN").and().and().build();
+
+    }
+
+    /**
+     * User Authorization mapping. Ex: only ADMIN users can create new projects and Employees
+     * */
+    @Bean
+    public SecurityFilterChain authorizationManager(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/employees/new").hasRole("ADMIN")
+                .antMatchers("/").authenticated().and().formLogin();
+        return http.build();
+    }
+
+}
