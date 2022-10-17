bf328a3ae6d20a233fa0b1ac597878ba7c951f6d
Yrol <yrollgayanth@yahoo.com>
2022-10-17 22:56:36 +1100

    Refactor project annotation test

--- src/test/java/com/yrol/pma/annotation/ProjectAnnotation.java
+++ src/test/java/com/yrol/pma/annotation/ProjectAnnotation.java
@@ -4,7 +4,6 @@ import com.yrol.pma.entities.Project;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
@@ -12,10 +11,8 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
 @SpringBootTest
-@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 public class ProjectAnnotation {
 
     @Autowired
